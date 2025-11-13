f2d5d44474d0833449faabf7f3e1d5c46641f35b
CiccioGM <ilqudi88@gmail.com>
2025-11-13 16:23:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,10 @@ if (!MONGO_URI) {
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("✅ Connessione a MongoDB riuscita"))
-  .catch((err) => console.error("❌ Errore connessione MongoDB:", err));
+  .catch((err) => {
+    console.error("❌ Errore connessione MongoDB:", err);
+    process.exit(1);
+  });
 
 // Rotte principali
 app.use("/api/transactions", transactionRoutes);
